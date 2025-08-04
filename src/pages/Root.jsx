e4ccb6bf00bf82c124c1e7c914d7fbbcaf24b904
MohammedAlaaDev <modi20272@gmail.com>
2025-08-04 13:46:39 +0400

    paper

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -14,7 +14,7 @@ export default function Root() {
             <AppBarComp drawerWidth={dw} />
             <DrawerComp drawerWidth={dw} />
 
-            <Box sx={{ml: dw, width: `calc(100% - ${dw})`, display: "flex", justifyContent: "center"}}>
+            <Box sx={{ ml: dw, mt: "50px", width: `calc(100% - ${dw})`, display: "flex", justifyContent: "center" }}>
                 <Outlet />
             </Box>
         </>
